da310d2ace46b8e821c0b3b6dca4254703340f12
Ross Patterson <me@rpatterson.net>
2019-08-05 00:47:20 -0700

    Project: Add project metadata

--- setup.py
+++ setup.py
@@ -1,21 +1,29 @@
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-your-username",
-    version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    name="rpatterson.media",
+    version="0.1",
+    author="Ross Patterson",
+    author_email="me@rpatterson.net",
+    description=(
+        "Media processing and management utilities, "
+        "keeping to the Unix DOTADIW philosophy"),
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    long_description_content_type="text/x-rst",
+    url="https://github.com/rpatterson/rpatterson.media",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: System Administrators",
+        "Natural Language :: English",
+        "Topic :: Multimedia",
+        "Topic :: Utilities",
     ],
 )
