a043b6975d36647164c5423285dc261677452d0e
Ross Patterson <me@rpatterson.net>
2019-08-14 21:51:12 -0700

    Convert: Add basic convert test

--- src/mediary/tests/test_convert.py
+++ src/mediary/tests/test_convert.py
@@ -2,7 +2,11 @@
 Tests for mediary conversion tools.
 """
 
+import os
+import tempfile
 import unittest
+
+from .. import convert
 
 
 class TestMediaryConvert(unittest.TestCase):
@@ -10,11 +14,16 @@ class TestMediaryConvert(unittest.TestCase):
     Test the mediary convert command.
     """
 
-    def test_tests(self):
+    INPUT_FILE = os.path.join(os.path.dirname(__file__), 'in.mp4')
+
+    def test_convert_basic(self):
         """
         Test that the tests run.
         """
-        self.assertTrue(True, 'Wrong boolean value')
+        with (
+                open(self.INPUT_FILE)) as input_file, (
+                tempfile.NamedTemporaryFile(mode='w')) as output_file:
+            convert.convert(input_file, output_file)
 
 
 if __name__ == '__main__':
